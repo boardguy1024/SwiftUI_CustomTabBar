1ce84766e2db95e780acc166fdaf7ca15a002db9
boardguy1024 <boardguy1024@gmail.com>
2021-07-07 23:03:16 +0900

    Initial Commit

---
+++ SwiftUI_CustomTabBar/SwiftUI_CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CustomTabBarApp.swift
+//  SwiftUI_CustomTabBar
+//
+//  Created by park kyung seok on 2021/07/07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
